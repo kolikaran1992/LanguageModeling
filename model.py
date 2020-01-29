378f15e01993e47116021bdfebc81e8ed23bacc6
AIMLTeam <kolikaran1992@gmail.com>
2020-01-29 18:28:21 +0530

    update model.py

--- model.py
+++ model.py
@@ -119,6 +119,10 @@ class LanguageModel(object):
 
         embedding_layer = self._get_embedding_layer(word_in, char_in, jtype_in)
         print(type(embedding_layer.shape[-1]))
+        print(dir(embedding_layer))
+        print(dir(embedding_layer.shape))
+        print(embedding_layer.get_shape().as_list())
+
 
         l2r_lstm = self._get_elmo_style_lstm_out(embedding_layer, name='l2r_lstm', rev=False, units=embedding_layer.shape[-1])
         r2l_lstm = self._get_elmo_style_lstm_out(embedding_layer, name='r2l_lstm', rev=True, units=embedding_layer.shape[-1])
