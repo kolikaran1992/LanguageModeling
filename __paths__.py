636d492af9d7441632cec1695c5a0a7618e07d27
AIMLTeam <kolikaran1992@gmail.com>
2020-01-29 17:10:40 +0530

    add gitkeep

--- __paths__.py
+++ __paths__.py
@@ -2,5 +2,5 @@ from pathlib import Path
 
 root = Path(__file__).parent
 path_to_logs = root.joinpath('logs')
-path_to_lm_wts = root.joinpath('weights', 'language_model')
+path_to_lm_wts = root.joinpath('weights')
 path_to_processor = root.joinpath('data_processor')
